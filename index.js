fc19f68f54fb5b5e5fdbd340b18ed5c20f99f109
CaioDetz <caiodetz654@gmail.com>
2023-11-24 17:56:40 +0000

    UP

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var tipo = document.getElementById("tipo");
 var form = document.getElementById("form");
 var result = document.getElementById("result");
 var back = document.getElementById("back");
-
+var mult = document.getElementById("mult");
 var items = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
 
 const zeroPad = (num, qty) => String(num).padStart(qty, "0");
@@ -15,9 +15,9 @@ btn.addEventListener("click", (e) => {
   e.preventDefault();
   const type = tipo.value;
   const count = parseInt(quant.value);
+  const m = parseInt(mult.value);
   form.classList.add("hidden");
   result.innerHTML = null;
-
   if (count < 100) {
     var pages = Math.ceil(count / 10);
     let r = 0;
@@ -39,16 +39,10 @@ btn.addEventListener("click", (e) => {
       result.innerHTML += `<div id="page${p}" class="grid grid-cols-2 gap-8 w-full h-screen"></div>`;
       for (let i = 0; i < items.length; i++) {
         let ep = document.getElementById(`page${p}`);
-        let n = 1 + p + items[i];
+        let n = 1 + p + items[i] + m - 1;
         let senha = zeroPad(n, 4);
         ep.innerHTML += `<div class="flex flex-col border border-black px-5 justify-center w-80 p-4 py-6 h-fit"><p class="text-2xl text-semibold ">Tipo de Atendimento:</p><p class="text-3xl font-bold text-black">${type}</p><div class=""></div><p class="text-xl text-semibold">Senha: <span class="text-3xl font-bold text-black">${senha}</span></p></div>`;
       }
     }
   }
 });
-
-// back.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   result.innerHTML = null;
-//   form.classList.remove("hidden");
-// });
